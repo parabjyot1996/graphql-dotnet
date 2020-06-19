b51f9a45933b10de6ace3c3140fda78eb2c20854
Parabjyot Singh <parabjyot1996@gmail.com>
2020-06-19 15:39:53 +0530

    Added graphql query for incoming request

---
+++ GraphQLServer/GraphQL/Queries/GraphQLQuery.cs
@@ -0,0 +1,15 @@
+using Newtonsoft.Json.Linq;
+
+namespace GraphQLServer.GraphQL.Queries
+{
+    public class GraphQLQuery
+    {
+        public string OperationName { get; set; }
+
+        public string NamedQuery { get; set; }
+
+        public string Query { get; set; }
+
+        public JObject Variables { get; set; }
+    }
+}

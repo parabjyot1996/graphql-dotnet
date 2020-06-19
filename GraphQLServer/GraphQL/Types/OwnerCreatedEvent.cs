983014d63281612539ed56e4e50f69b501cb2caf
Parabjyot Singh <parabjyot1996@gmail.com>
2020-06-19 15:35:15 +0530

    Added dependency injection for DataLoader

--- GraphQLServer/GraphQL/Types/OwnerCreatedEvent.cs
+++ GraphQLServer/GraphQL/Types/OwnerCreatedEvent.cs
@@ -1,10 +1,12 @@
+using GraphQL.DataLoader;
 using GraphQLServer.Contracts;
 
 namespace GraphQLServer.GraphQL.Types
 {
     public class OwnerCreatedEvents : OwnerType
     {
-        public OwnerCreatedEvents(IAccountRepository repository) : base(repository)
+        public OwnerCreatedEvents(IAccountRepository repository, IDataLoaderContextAccessor dataLoader) 
+            : base(repository, dataLoader)
         {
         }
     }

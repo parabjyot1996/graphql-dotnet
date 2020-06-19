deb502cf8552472be631358eb7ba82c0317fe497
Parabjyot Singh <parabjyot1996@gmail.com>
2020-06-19 15:36:30 +0530

    Added dependency injection for DataLoader

--- GraphQLServer/GraphQL/Types/OwnerType.cs
+++ GraphQLServer/GraphQL/Types/OwnerType.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using GraphQL.DataLoader;
 using GraphQL.Types;
 using GraphQLServer.Contracts;
 using GraphQLServer.Entities;
@@ -7,14 +9,21 @@ namespace GraphQLServer.GraphQL.Types
 {
     public class OwnerType : ObjectGraphType<Owner>
     {
-        public OwnerType(IAccountRepository repository)
+        public OwnerType(IAccountRepository repository,
+                            IDataLoaderContextAccessor dataLoader)
         {
             Field(x => x.Id, type: typeof(IdGraphType)).Description("Id property from owner object");
             Field(x => x.Name).Description("Name property from the owner object");
             Field(x => x.Address).Description("Address property from the owner object.");
             Field<ListGraphType<AccountType>>(
                 "accounts",
-                resolve: context => repository.GetAccountByOwnerId(context.Source.Id)
+                //resolve: context => repository.GetAccountByOwnerId(context.Source.Id)
+                resolve: context => 
+                {
+                    var loader = dataLoader.Context
+                                    .GetOrAddCollectionBatchLoader<int, Account>("GetAccountsByOwnerIds", repository.GetAccountsByOwnerIds);
+                    return loader.LoadAsync(context.Source.Id);
+                }
             );
         }
     }
